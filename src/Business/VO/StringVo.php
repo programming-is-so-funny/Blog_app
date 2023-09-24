9ee0070d6eb05d655efa92efc47eb8debf2a6f31
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2023-09-25 00:01:57 +0100

    tests coverage for throw not empty exception in vo

--- src/Business/VO/StringVo.php
+++ src/Business/VO/StringVo.php
@@ -2,18 +2,34 @@
 
 namespace App\Business\VO;
 
+use App\Business\Utils\Exceptions\NotEmptyException;
+
 class StringVo
 {
     protected string $value;
 
+    /**
+     * @throws NotEmptyException
+     */
     public function __construct(string $value)
     {
+        $this->validate($value);
         $this->value = $value;
     }
 
     public function value(): string
     {
         return $this->value;
+    }
+
+    /**
+     * @throws NotEmptyException
+     */
+    private function validate(string $value): void
+    {
+        if (empty(trim($value))){
+            throw new NotEmptyException("Cette valeur n'est pas valide !");
+        }
     }
 
 }
