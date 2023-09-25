9bfd7b06ff2ab8a51caa2111c6f7dc8717a4b42e
Geekers-Joel237 <79468070+Geekers-Joel237@users.noreply.github.com>
2023-09-25 03:38:13 +0100

    add tests for db connection

---
+++ Tests/Unit/DBConnectionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Business\Utils\Database\DBConnection;
+use App\Business\Utils\Exceptions\ErrorOnConnectToDatabaseException;
+use App\Database\PdoConnection;
+use PHPUnit\Framework\TestCase;
+
+class DBConnectionTest extends TestCase
+{
+    private DBConnection $connection;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+    }
+
+    /**
+     * @throws ErrorOnConnectToDatabaseException
+     */
+    public function test_can_connect_to_db()
+    {
+        $this->connection = new PdoConnection(
+            driver: $_ENV['DB_CONNECTION'],
+            host: $_ENV['DB_HOST'],
+            port: $_ENV['DB_PORT'],
+            dbName: $_ENV['DB_DATABASE'],
+            userName: $_ENV['DB_USERNAME'],
+            password: $_ENV['DB_PASSWORD'],
+        );
+        $this->assertNotNull($this->connection->getConnection());
+    }
+
+    public function test_can_throw_error_on_connect_to_db_exception()
+    {
+        $this->expectException(ErrorOnConnectToDatabaseException::class);
+        $this->connection = new PdoConnection(
+            driver: "",
+            host: "",
+            port: "",
+            dbName: "",
+            userName: "",
+            password: "",
+        );
+
+        $this->connection->getConnection();
+    }
+}
